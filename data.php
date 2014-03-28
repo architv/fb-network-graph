2d05a4ffc3bb537272c93828cf53ea8501802a05
Archit Verma <architv07@gmail.com>
2014-03-28 20:58:28 +0530

    minor change

--- data.php
+++ data.php
@@ -1,5 +1,5 @@
 <?php
-	$data = file_get_contents("https://graph.facebook.com/870175569_10153929919780570?fields=likes.fields(pic_square,name).limit(100),comments&access_token=CAACEdEose0cBALmAKRgU70JJG4dbqmhUxu0Vd8ANmlZCKcpl8kTorbm1KI5jO1pOrlcGQ0fuOJLqMFqalpUHZBHHrEbZB41ki1D0ZCNLEJ1wZBZBnlTaNbGVhjHVbjproISc5p7UYbOqnT1npMRrLJM46XJdEDURG3fVykGhnOOQ83l2tkFWlcYFPDqqKIFR8ZD"); //change the access token here
+	$data = file_get_contents("https://graph.facebook.com/870175569_10153929919780570?fields=likes.fields(pic_square,name).limit(100),comments&access_token=access_token_here"); //change the access token here
 	$likes = json_decode($data);
 	$real_likes = $likes->likes->data;
 	$no = count($real_likes);
